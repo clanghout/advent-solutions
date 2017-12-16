e3382205f649714ba169fd13c8491c031695e953
Chris Langhout <c.e.i.langhout@student.tudelft.nl>
2017-12-16 15:06:07 +0100

    day 10 part 2

--- adventDay10.js
+++ adventDay10.js
@@ -1,64 +1,75 @@
-let input10 = [102, 255, 99, 252, 200, 24, 219, 57, 103, 2, 226, 254, 1, 0, 69, 216];
-let testIn = [3, 4, 1, 5];
+const input10 = "102,255,99,252,200,24,219,57,103,2,226,254,1,0,69,216";
+const testIn = "1,2,3";
 
 function computeAdvent10(inp, size) {
     if (!size) {
         size = 256;
     }
 
+    let asciiInput = inp.split('').map(a => a.charCodeAt(0));
+    const defaultAppend = [17, 31, 73, 47, 23];
+    asciiInput = _.concat(asciiInput,defaultAppend);
+
     // create array from 0 to size
     let curList = [...Array(size).keys()];
     let skipSize = 0;
     let curPos = 0;
 
-    for (let i = 0; i < inp.length; i++) {
-        // console.log('i', inp[i]);
-        curList = doHashRound(curList, inp[i], curPos);
-        // console.log("newList",curList);
-        // console.log(`${curPos} = (${inp[i]} + ${skipSize}) % ${curList.length};`)
-        curPos = (curPos + (inp[i] + skipSize)) % curList.length;
-        // console.log(`new curpos ${curPos}`);
-        skipSize++;
+    for(let round = 0;round<64;round++){
+        for (let i = 0; i < asciiInput.length; i++) {
+            curList = doHashRound(curList, asciiInput[i], curPos);
+            curPos = (curPos + (asciiInput[i] + skipSize)) % curList.length;
+            skipSize++;
+        }
     }
-    return curList[0]*curList[1];
+
+    let charList = [];
+    while(curList.length > 0){
+        charList.push(curList.splice(0,16));
+    }
+    const hashChars = charList.map(a => computeXor(a).toString(16));
+
+    return hashChars.reduce((acc, curr) => {
+            if (curr.length === 1) {
+                acc += "0"
+            }
+            acc += curr;
+            return acc;
+        }
+    );
 }
 
 function doHashRound(curState, i, curPos) {
-    // console.log(`beginState, i ${i}, curpos ${curPos}`,curState);
     let startLength = curState.length;
     let toRotate = curState.splice(curPos,i);
     // fill toRotate with the rest...
     if(toRotate.length < i){
 
         let extraRotate = curState.splice(0,i-toRotate.length);
-        // console.log(`first rotate ${toRotate}, extra ${extraRotate}`);
         toRotate = _.concat(toRotate,extraRotate);
     }
-    // console.log(`before Rotate ${toRotate}`)
     toRotate.reverse();
-    // console.log('Rotate',toRotate);
-    // console.log('curState',curState);
     let result = curState.slice();
-    // console.log("result",result);
-    //TODO for loop needs to start at correct index
     for(let j = curPos;j<(curPos+toRotate.length);j++){
-        // debugger;
         let rotateI = j-curPos;
-        // console.log(`beforeAdding j=${j} startLength=${startLength}`)
         if(j<startLength){
             // Add to the position j until end of 'startList' is reached.
-            // console.log("default adding");
             result.splice(j,0,toRotate[rotateI]);}
         else{
-            // console.log("adding to front");
             // Add to start of list, because of loop
             result.splice(j-(startLength),0,toRotate[rotateI]);
         }
-        // console.log("resulti",result);
     }
-    // console.log("result2",result);
     return result;
 }
 
-console.log(`Test with list size 5 should give 12: ${computeAdvent10(testIn, 5)}`);
 testAdvent(input10, false, computeAdvent10);
+
+function computeXor(list) {
+    let res = 0;
+    list.forEach(a => res = res ^ a);
+    return res;
+}
+
+let testXor = [65 , 27 , 9 , 1 , 4 , 3 , 40 , 50 , 91 , 7 , 6 , 0 , 2 , 5 , 68 , 22];
+console.log(`text xor ${computeXor(testXor)} : 64`);
